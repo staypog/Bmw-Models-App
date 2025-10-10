664af8eb4dd268e9d05572295b434cfdcc2e93c7
George <starantsovgeorge@gmail.com>
2025-10-10 16:43:06 +0300

    Initial Commit

---
+++ Bmw Models feat.George 10 oct/Bmw_Models_feat_George_10_octApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bmw_Models_feat_George_10_octApp.swift
+//  Bmw Models feat.George 10 oct
+//
+//  Created by George Starantsov on 10.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Bmw_Models_feat_George_10_octApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bmw Models feat.George 10 oct/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bmw Models feat.George 10 oct
+//
+//  Created by George Starantsov on 10.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
